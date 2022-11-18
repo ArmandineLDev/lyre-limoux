901b4e374126418f7ab0ccbe587322f8bce38987
Armandine BARGE <armandine.barge@ntymail.com>
2022-11-18 19:49:52 +0100

    mentions légales 2

--- client/pages/mentions-legales/index.js
+++ client/pages/mentions-legales/index.js
@@ -2,22 +2,42 @@ const MentionLegales = () => {
   return (
     <div>
       <h2>Mentions Légales</h2>
-      <p>Nom de l'association : Lyre de Limoux</p>
-      <p>Adresse : 14 rue du Maquis -11300 Limoux</p>
-      <p>Téléphone : 04-68-31-55-05</p>
-      <p>Directeur de Publication : Monsieur Jacques Salasar, Président</p>
+      <p>
+        <b>Nom de l'association :</b> Lyre de Limoux
+      </p>
+      <p>
+        <b>Association Loi 1901, enregistrée sour le numéro :</b>
+      </p>
+      <p>
+        <b>Adresse :</b> 14 rue du Maquis -11300 Limoux
+      </p>
+      <p>
+        <b>Téléphone :</b> 04-68-31-55-05
+      </p>
+      <p>
+        <b>Directeur de Publication :</b> Monsieur Jacques Salasar, Président
+      </p>
       <div>
-        <p>Hébergeur :VERCEL</p>
         <p>
-          Adresse web :{" "}
+          <b>Hébergeur : </b>VERCEL
+        </p>
+        <p>
+          <b>Adresse web :</b>
           <Link href="www.vercel.com">
             <a>www.vercel.com</a>
           </Link>
         </p>
-        <p>Adresse Postale : Vercel Inc</p>
+        <p>
+          <b>Adresse Postale :</b> Vercel Inc
+        </p>
         <p>340 S Lemon Ave #4133</p>
         <p>Walnet, CA 91789</p>
-        <p>Numéro de téléphone : </p>
+        <p>
+          <b>Numéro de téléphone :</b> (559) 288-7060
+        </p>
+        <p>
+          <b>Email :</b> privacy@vercel.com
+        </p>
       </div>
     </div>
   );
